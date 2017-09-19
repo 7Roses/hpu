fdc27c2c6de6c79797f3d2ad766a039f5c50664e
7Roses <rijckdejoris@hotmail.com>
2017-09-19 21:24:01 +0200

    refactor to use the vector api for this

--- usr/lib/hpudraw.lua
+++ usr/lib/hpudraw.lua
@@ -9,29 +9,20 @@ hpudraw.func = function(Fx,Fy,Fz,value,stepSize)
 end;
 
 hpudraw.line = function(beginPoint,endPoint,value)
-	-- find the equations:
-	local beginV = vector:new(beginPoint);
-	local endV = vector:new(endPoint);
-	local totalV = endV:subtract(beginV);
-
-	local f = function(t,ind) return beginV.value[ind] + (totalV.value[ind] * t) end;
-	
-	-- find the minimum step size
-	local maxima = 0;
-	for k,v in pairs(totalV.value) do
-	  if v>maxima then maxima=v end
-	end;
-	local stepSize = (1.0 / maxima)/2 ;
-	
-	-- run the stepsize till 1
-	hpudraw(
-	function(i) f(i,"x") end,
-	function(i) f(i,"y") end,
-	function(i) f(i,"z") end,
-	value,stepSize
-	)
+	local p1 = vector:new(beginPoint);
+	local p2 = vector:new(endPoint);
+	local ftable = vector.math.lineEquationTable(p1,p2);
+	hpudraw.func(ftable.x, ftable.y, ftable.z, value,ftable.step);
 end;
 
-
+hpudraw.vertex = function(p1,p2,p3,value) 
+	checkArg(1,p1,"table");
+	checkArg(2,p2,"table");
+	checkArg(3,p3,"table");
+	checkArg(4,value,"number","nil");
+	
+	
+	
+end;
 
 return hpudraw;
