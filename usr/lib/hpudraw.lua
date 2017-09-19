eaa48e9a1ce81803285856499b8b500df5214a38
7Roses <rijckdejoris@hotmail.com>
2017-09-19 22:03:18 +0200

    add vertex drawing. ! be aware its slow

--- usr/lib/hpudraw.lua
+++ usr/lib/hpudraw.lua
@@ -20,9 +20,12 @@ hpudraw.vertex = function(p1,p2,p3,value)
 	checkArg(2,p2,"table");
 	checkArg(3,p3,"table");
 	checkArg(4,value,"number","nil");
-	
-	
-	
+	local v1,v2,v3 = vector:new(p1),vector:new(p2),vector:new(p3);
+	local fp12 = vector.math.lineEquationTable(v1,v2);
+	for i=0.0,1.0+fp12.step,fp12.step do
+		local vt = {x=fp12.x(i),y=fp12.y(i),z=fp12.z(i)};
+		hpudraw.line(vt,v3.value,value);
+	end
 end;
 
 return hpudraw;
